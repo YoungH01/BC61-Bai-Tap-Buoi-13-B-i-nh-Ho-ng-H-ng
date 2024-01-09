65673a2c8ab194ede2c9a9b186a2127c120dbb64
heung <hoanghung.bkevent@gmail.com>
2024-01-09 09:47:24 +0700

    demo

---
+++ script.js
@@ -0,0 +1,14 @@
+function demo(){
+    document.getElementById("demo1").style.backgroundColor="#106CF6";
+    document.getElementById("demo1").style.color="#fff";
+}
+function Bai2(){
+    // var document.getElementById("")
+    
+}
+function Bai3(){
+}
+function Bai4(){
+}
+function Bai5(){
+}
